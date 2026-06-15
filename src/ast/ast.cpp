e221a2c386b608897438bc0b1244941b3a18da55
venomous-maker <morganokumu8+1@gmail.com>
2026-06-15 13:38:13 +0300

    Add AST helper definitions

---
+++ src/ast/ast.cpp
@@ -0,0 +1,50 @@
+#include "ast/ast.h"
+
+namespace grav {
+
+const char *binaryOpSymbol(BinaryOp op) {
+    switch (op) {
+        case BinaryOp::Add: return "+";
+        case BinaryOp::Sub: return "-";
+        case BinaryOp::Mul: return "*";
+        case BinaryOp::Div: return "/";
+        case BinaryOp::Eq: return "==";
+        case BinaryOp::NotEq: return "!=";
+        case BinaryOp::Greater: return ">";
+        case BinaryOp::Less: return "<";
+        case BinaryOp::GreaterEq: return ">=";
+        case BinaryOp::LessEq: return "<=";
+        case BinaryOp::And: return "&&";
+        case BinaryOp::Or: return "||";
+    }
+    return "?";
+}
+
+bool isComparison(BinaryOp op) {
+    switch (op) {
+        case BinaryOp::Eq:
+        case BinaryOp::NotEq:
+        case BinaryOp::Greater:
+        case BinaryOp::Less:
+        case BinaryOp::GreaterEq:
+        case BinaryOp::LessEq:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool isLogical(BinaryOp op) {
+    return op == BinaryOp::And || op == BinaryOp::Or;
+}
+
+const char *accessName(Access a) {
+    switch (a) {
+        case Access::Public: return "public";
+        case Access::Private: return "private";
+        case Access::Protected: return "protected";
+    }
+    return "public";
+}
+
+} // namespace grav
