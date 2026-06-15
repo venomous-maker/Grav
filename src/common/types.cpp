96d6d23c81303438295855153338177cabdafc9c
venomous-maker <morganokumu8+1@gmail.com>
2026-06-15 13:38:13 +0300

    Add TypeRef name helper

---
+++ src/common/types.cpp
@@ -0,0 +1,18 @@
+#include "common/types.h"
+
+namespace grav {
+
+std::string typeRefName(const TypeRef &t) {
+    switch (t.kind) {
+        case TypeRef::Kind::Int: return "int";
+        case TypeRef::Kind::Float: return "float";
+        case TypeRef::Kind::Bool: return "bool";
+        case TypeRef::Kind::String: return "string";
+        case TypeRef::Kind::Void: return "void";
+        case TypeRef::Kind::Named: return t.name;
+        case TypeRef::Kind::Error: return "<error>";
+    }
+    return "<error>";
+}
+
+} // namespace grav
